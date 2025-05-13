8d0194aae4951aa07eb7d828463dea56f30ce2ba
Henrique Biruel Furtado <202210347@al.facamp.com.br>
2025-05-13 14:43:55 -0300

    Corrigindo submódulos e adicionando pastas como diretórios normais

---
+++ trabalho_backend/app/__init__.py
@@ -0,0 +1,14 @@
+from config import Config
+from flask import Flask
+from flask_pymongo import PyMongo
+from flask_cors import CORS, cross_origin
+
+app = Flask(__name__)
+app.config.from_object(Config)
+
+CORS(app, resources={r'/*': {'origins': '*'}})
+
+mongodb_client = PyMongo(app, uri="mongodb+srv://202210347:k15072005@cluster0.2hoki.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+db = mongodb_client.db
+
+from app import routes
